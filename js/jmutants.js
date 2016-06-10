e634d3dc6e4689bd9625c65c5fd6766a14951b8d
Kenneth Tam <ktam@purdue.edu>
2016-06-10 11:45:31 -0400

    Adds your insult to the mutant API whenever it's created

--- js/jmutants.js
+++ js/jmutants.js
@@ -30,13 +30,52 @@ var jMutants = {
     var f = ev.currentTarget;
     this.insultList.append(this.buildListItem({
       studentName: $(f.student_select.selectedOptions).val(),
-      insult: f.insult_select.value
+      starterInsult:f.insult_starter.value,
+      insult: f.insult_select.value,
+      additionalInsult: f.insult_additional.value
     }));
   },
 
   buildListItem: function(insult) {
     var li = this.insultListTemplate.clone();
-    li.find('.student-name').text(insult.studentName + ' ' + insult.insult);
+    var finalInsult;
+    if(insult.starterInsult && insult.additionalInsult) {
+      finalInsult = insult.studentName + ' ' + insult.starterInsult + ' and ' + insult.additionalInsult + '.';
+      li.find('.student-name').text(finalInsult);
+    }
+    else if(insult.starterInsult) {
+      finalInsult = insult.studentName + ' ' + insult.starterInsult + '.';
+      li.find('.student-name').text(finalInsult);
+    }
+    else if(insult.insult && insult.additionalInsult) {
+      finalInsult = insult.studentName + ' ' + insult.insult + ' and ' + insult.additionalInsult + '.';
+      li.find('.student-name').text(finalInsult);
+    }
+    else {
+      finalInsult = insult.studentName + ' ' + insult.insult + '.';
+      li.find('.student-name').text(finalInsult);
+    }
+
+
+    jQuery.ajax({
+        url: "https://mutant-school.herokuapp.com/api/v1/mutants",
+        type: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        contentType: "application/json",
+        data: JSON.stringify({
+            "mutant": {
+                "eligibility_begins_at": "-",
+                "power": "-",
+                "may_advise_beginning_at": "-",
+                "eligibility_ends_at": "-",
+                "real_name": "-",
+                "mutant_name": finalInsult
+            }
+        })
+    });
+
     return li.removeClass('hide');
   },
 
